a6411b3766a8b27cac58011cdb3601f64bb46d3d
uche-inyama <depoulet@yahoo.com>
2022-02-25 14:18:04 +0000

    testing library feature: get_user_library after 2 minutes

--- app/models/library.rb
+++ app/models/library.rb
@@ -5,5 +5,5 @@ class Library < ApplicationRecord
   validates :status, presence: true
   validates :user_id, presence: true
 
-  scope :get_user_library, -> { self.all.where('created_at > ?', Time.now - 48.hours ).order(created_at: :asc) }
+  scope :get_user_library, -> { self.all.where('created_at > ?', Time.now - 2.minutes ).order(created_at: :asc) }
 end
